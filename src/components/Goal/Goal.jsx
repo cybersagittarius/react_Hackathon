82758e137bef3691ac4711b34d0a0a65323ac907
Andreas <volosh.andrey@gmail.com>
2021-02-26 14:04:09 +0100

    assets and scratch of components

---
+++ src/components/Goal/Goal.jsx
@@ -0,0 +1,25 @@
+export default function (props) {
+    return (
+        <div id="goal">
+            <title>
+                Your new life will go smoother in just few steps with “Helping Hands”
+      </title>
+            <img src = "./assets/pic.jpg" alt="" />
+            <div>
+                <div>
+                    <button/>
+                    <p>Select your Region &amp; City</p>
+                </div>
+                <div>
+                    <button/>
+                    <p>Select the category you seek help in</p>
+                </div>
+                <div>
+                    <button/>
+                    <p>Find the support you need</p>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
