e0db4c01888be1278b6fc828798e93fa8e05787c
Andreas <volosh.andrey@gmail.com>
2021-02-26 16:47:49 +0100

    goal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import About from './components/About/About'
+import Goal from './components/Goal/Goal'
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <About />
+      <Goal/>
       
     </div>
   );
--- src/components/Goal/Goal.jsx
+++ src/components/Goal/Goal.jsx
@@ -1,23 +1,30 @@
+import './Goal.css'
+
+
 export default function (props) {
     return (
         <div id="goal">
             <title>
                 Your new life will go smoother in just few steps with “Helping Hands”
-      </title>
-            <img src = "./assets/pic.jpg" alt="" />
-            <div>
-                <div>
-                    <button/>
-                    <p>Select your Region &amp; City</p>
+            </title>
+            <img id ="main-pic" src = "./assets/pic.jpg" alt="" />
+            <div id="steps">
+                <div className = "pass">
+                    <img className ="number" src="./assets/numbers/1.png" alt=""/>
+                    <p className = "step">Select your Region & City</p>
                 </div>
-                <div>
-                    <button/>
-                    <p>Select the category you seek help in</p>
+                
+                <div className = "pass">
+                    <img className ="number" src="./assets/numbers/2.png" alt=""/>
+                    <p className = "step">Select the category you seek help in</p></div>
+                <div className = "pass">
+                    <img className ="number" src="./assets/numbers/3.png" alt=""/>
+                    <p className = "step">Find the support you need</p>
                 </div>
-                <div>
-                    <button/>
-                    <p>Find the support you need</p>
-                </div>
+               
+                
+                
+                
             </div>
         </div>
         
